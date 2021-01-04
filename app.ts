1916848541bd3bee471ec33ad2b95d1e8090f5db
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-01-04 22:28:41 +0100

    type never

--- app.ts
+++ app.ts
@@ -6,3 +6,9 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     username = userInput;
 }
+
+function generateError(message: string, code: number){
+    throw {message: message, errorCode: code};
+}
+
+generateError('An error occurred',500);
