3302957729faf9e396c7d853710870e9dd09a663
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-01-04 22:21:14 +0100

    unknown type

--- app.ts
+++ app.ts
@@ -1,24 +1,8 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+let userInput: unknown;
+let username: string;
+
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    username = userInput;
 }
-
-function printResult(num: number) {
-    console.log('Result: ' + num);
-}
-
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-    const result = n1 + n2;
-    cb(result);
-}
-
-printResult(add(5, 12))
-
-let combineValues: (x: number, y: number) => number;
-
-combineValues = add;
-
-console.log(combineValues(8, 8));
-
-addAndHandle(10, 20, (result) => {
-    console.log('Result: ' + result);
-})
