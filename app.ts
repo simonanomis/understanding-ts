5de73636c7954ebf684eac8ebca3b839acafc484
Simona Stevkovska <s.stevkovska@linkplus-it.com>
2021-01-04 21:45:54 +0100

    custom type aliases

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
-function combine(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
     let result;
     if(typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
         result = +input1 + +input2; //each input with + is converted to a number befoe combine
